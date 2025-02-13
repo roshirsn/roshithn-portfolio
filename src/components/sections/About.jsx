53a7619481a4a0357ebfe8ab8404df468626f71c
roshirsn <roshirsn27@gmail.com>
2025-02-13 19:15:25 +0530

    Initial commit

---
+++ src/components/sections/About.jsx
@@ -0,0 +1,145 @@
+import { RevealOnScroll } from "../RevealOnScroll";
+
+export const About = () => {
+  const frontendSkills = [
+    "React",
+    "Vue",
+    "JavaScript",
+    "TailwindCSS",
+    "Bootstrap",
+  ];
+
+  const backendSkills = ["Node.js", "Python", "Flask", "MongoDB", "SQL"];
+  const Prolanguages = ["C", "Python", "JavaScript", "Go", "Dart"];
+  const MLskills = ["TensorFlow", "PyTorch", "CNN", "Yolo", "Monai"];
+  return (
+    <section
+      id="about"
+      className="min-h-screen flex items-center justify-center py-20"
+    >
+      <RevealOnScroll>
+        <div className="max-w-3xl mx-auto px-4">
+          <h2 className="text-3xl font-bold mb-8 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent text-center">
+            {" "}
+            About Me
+          </h2>
+
+          <div className="rounded-xl p-8 border-white/10 border hover:-translate-y-1 transition-all">
+            <p className="text-gray-300 mb-6">
+              Passionate developer with expertise in building scalable AI models and creating innovative solutions.
+            </p>
+
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+            
+
+                <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
+                <h3 className="text-xl font-bold mb-4"> languages</h3>
+                <div className="flex flex-wrap gap-2">
+                  {Prolanguages.map((tech, key) => (
+                    <span
+                      key={key}
+                      className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 
+                                    hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition
+                    "
+                    >
+                      {tech}
+                    </span>
+                  ))}
+                </div>
+              </div>
+
+              <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
+                <h3 className="text-xl font-bold mb-4"> AI/ML</h3>
+                <div className="flex flex-wrap gap-2">
+                  {MLskills.map((tech, key) => (
+                    <span
+                      key={key}
+                      className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 
+                                    hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition
+                    "
+                    >
+                      {tech}
+                    </span>
+                  ))}
+                </div>
+              </div>
+
+
+              <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
+                <h3 className="text-xl font-bold mb-4"> Frontend</h3>
+                <div className="flex flex-wrap gap-2">
+                  {frontendSkills.map((tech, key) => (
+                    <span
+                      key={key}
+                      className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 
+                                    hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition
+                    "
+                    >
+                      {tech}
+                    </span>
+                  ))}
+                </div>
+              </div>
+
+              <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
+                <h3 className="text-xl font-bold mb-4"> Backend</h3>
+                <div className="flex flex-wrap gap-2">
+                  {backendSkills.map((tech, key) => (
+                    <span
+                      key={key}
+                      className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 
+                                    hover:shadow-[0_2px_8px_rgba(59,130,2246,0.2)] transition
+                    "
+                    >
+                      {tech}
+                    </span>
+                  ))}
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-8">
+            <div className="p-6 rounded-xl border-white/10 border hover:-translate-y-1 transition-all">
+              <h3 className="text-xl font-bold mb-4"> 🏫 Education </h3>
+              <ul className="list-disc list-inside text-gray-300 space-y-2">
+                <li>
+                  <strong> B.Tech in Computer Science and Engineering </strong> - APJ Abdul Kalam Technological University.
+                  (2016-2020)
+                </li>
+                <li>
+                  Relevant Coursework: Data Structures, Web Development, Cyber Security, Machine Learning...
+                </li>
+              </ul>
+            </div>
+            <div className="p-6 rounded-xl border-white/10 border hover:-translate-y-1 transition-all">
+              <h3 className="text-xl font-bold mb-4"> 💼 Work Experience </h3>
+              <div className="space-y-4 text-gray-300">
+                <div>
+                  <h4 className="font-semibold">
+                    {" "}
+                    AI Engineer Trainee at CDAC, Trivandrum (2024 - Present){" "}
+                  </h4>
+                  <p>
+                    Designed and developed AI Driven Breast Cancer Detection System using Deep Learning, integrated models into production via optimized backend APIs.
+                  </p>
+                </div>
+
+                {/* <div>
+                  <h4 className="font-semibold">
+                    {" "}
+                    Intern at DEF Startups (2019){" "}
+                  </h4>
+                  <p>
+                    Assisted in building front-end components and integration
+                    REST APIs
+                  </p>
+                </div> */}
+              </div>
+            </div>
+          </div>
+        </div>
+      </RevealOnScroll>
+    </section>
+  );
+};
---
+++ src/components/sections/Projects.jsx
@@ -0,0 +1,168 @@
+import { RevealOnScroll } from "../RevealOnScroll";
+
+export const Projects = () => {
+  return (
+    <section
+      id="projects"
+      className="min-h-screen flex items-center justify-center py-20"
+    >
+      <RevealOnScroll>
+        <div className="max-w-5xl mx-auto px-4">
+          <h2 className="text-3xl font-bold mb-8 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent text-center">
+            {" "}
+            Featured Projects
+          </h2>
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+            <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">
+
+
+              <h3 className="text-xl font-bold mb-2"> AI Driven Breast Cancer Detection</h3>
+              <p className="text-gray-400 mb-4">
+              Designed a comprehensive Breast Cancer Detection System that integrates image classification and
+              object detection pipelines for cancer diagnosis.
+              </p>
+              <div className="flex flex-wrap gap-2 mb-4">
+                {["Python", "TensorFlow", "Keras", "Gradio", "Yolov8"].map((tech, key) => (
+                  <span
+                    key={key}
+                    className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 
+                                    hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)] transition-all
+                    "
+                  >
+                    {tech}
+                  </span>
+                ))}
+              </div>
+
+              <div className="flex justify-between items-center">
+                <a
+                  href="#"
+                  className="text-blue-400 hover:text-blue-300 transition-colors my-4"
+                >
+                  View Project →
+                </a>
+              </div>
+            </div>
+            <div
+              className="
+              glass p-6 rounded-xl border border-white/10 
+              hover:-translate-y-1 hover:border-blue-500/30
+              hover:shadow-[0_4px_20px_rgba(59,130,246,0.1)]
+              transition-all
+            "
+            >
+              <h3 className="text-xl font-bold mb-2">Personal Portfolio</h3>
+              <p className="text-gray-400 mb-4">
+              A responsive, single-page application built with React and styled with Tailwind CSS to showcase my skills and projects in a clean and engaging format.
+              </p>
+              <div className="flex flex-wrap gap-2 mb-4">
+                {["Javascript", "React", "Tailwind CSS", "Email JS"].map((tech, key) => (
+                  <span
+                    key={key}
+                    className="
+                      bg-blue-500/10 text-blue-500 py-1 px-3 
+                      rounded-full text-sm
+                      transition
+                      hover:bg-blue-500/20 hover:-translate-y-0.5
+                      hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)]
+                    "
+                  >
+                    {tech}
+                  </span>
+                ))}
+              </div>
+              <div className="flex justify-between items-center">
+                <a
+                  href="#"
+                  className="text-blue-400 hover:text-blue-300 transition-colors my-4"
+                >
+                  View Project →https://roshirsn.github.io/roshithn-portfolio
+                </a>
+              </div>
+            </div>
+
+            <div
+              className="
+              glass p-6 rounded-xl border border-white/10 
+              hover:-translate-y-1 hover:border-blue-500/30
+              hover:shadow-[0_4px_20px_rgba(59,130,246,0.1)]
+              transition-all
+            "
+            >
+              <h3 className="text-xl font-bold mb-2">Spread: Connect with you</h3>
+              <p className="text-gray-400 mb-4">
+              Developed an Android application for college events, news, and updates. The app aims to enhance
+              connectivity and engagement within the college community by providing timely and relevant information.
+              </p>
+              <div className="flex flex-wrap gap-2 mb-4">
+                {["Flutter", "Dart", "Firebase"].map(
+                  (tech) => (
+                    <span
+                      key={tech}
+                      className="
+                      bg-blue-500/10 text-blue-500 py-1 px-3 
+                      rounded-full text-sm
+                      transition
+                      hover:bg-blue-500/20 hover:-translate-y-0.5
+                      hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)]
+                    "
+                    >
+                      {tech}
+                    </span>
+                  )
+                )}
+              </div>
+              <div className="flex justify-between items-center">
+                <a
+                  href="#"
+                  className="text-blue-400 hover:text-blue-300 transition-colors my-4"
+                >
+                  View Project →
+                </a>
+              </div>
+            </div>
+
+            <div
+              className="
+              glass p-6 rounded-xl border border-white/10 
+              hover:-translate-y-1 hover:border-blue-500/30
+              hover:shadow-[0_4px_20px_rgba(59,130,246,0.1)]
+              transition-all
+            "
+            >
+              <h3 className="text-xl font-bold mb-2">SocialBook - Inspiration from Facebook</h3>
+              <p className="text-gray-400 mb-4">
+              Developed SocialBook, a responsive social media platform inspired by Facebook, using HTML, CSS, and
+              JavaScript. Implemented user profiles, posts, and light/dark modes for a seamless cross-device experience
+              </p>
+              <div className="flex flex-wrap gap-2 mb-4">
+                {["HTML", "CSS", "JavaScript"].map((tech, key) => (
+                  <span
+                    key={key}
+                    className="
+                      bg-blue-500/10 text-blue-500 py-1 px-3 
+                      rounded-full text-sm
+                      transition
+                      hover:bg-blue-500/20 hover:-translate-y-0.5
+                      hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)]
+                    "
+                  >
+                    {tech}
+                  </span>
+                ))}
+              </div>
+              <div className="flex justify-between items-center ">
+                <a
+                  href="#"
+                  className="text-blue-400 hover:text-blue-300 transition-colors my-4"
+                >
+                  View Project →http://socialbook-24.s3-website.us-east-2.amazonaws.com/
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </RevealOnScroll>
+    </section>
+  );
+};
